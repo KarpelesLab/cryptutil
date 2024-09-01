4ad306cbe35e150f8caf998504ca2ea2d5b774c3
Mark Karpeles <magicaltux@gmail.com>
2024-09-01 14:22:47 +0900

    add a type for PrivateKey

--- keys.go
+++ keys.go
@@ -2,6 +2,13 @@ package cryptutil
 
 import "crypto"
 
+// PrivateKey represents a private key using an unspecified algorithm.
+//
+// All private keys must implement a method to retrieve the matching public key.
+type PrivateKey interface {
+	Public() crypto.PublicKey
+}
+
 // PublicKey returns the public key for a given private key, or nil if the argumlent is not a private
 // key or if its Public() method returned nil.
 func PublicKey(privKey crypto.PrivateKey) crypto.PublicKey {
